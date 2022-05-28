0dd8cf41535405c1cf54d694413d216660489123
flengure <flengure@gmail.com>
2022-05-28 09:42:12 +0100

    documentation

--- src/number_factory.rs
+++ src/number_factory.rs
@@ -3,74 +3,70 @@
     #[derive(Debug)]
     pub struct NumberFactory<'a> {
         pub input: &'a str,
-        dictionary: [&'a str; 37],
-        tens_index: usize,
-        huns_index: usize,
+        dictionary: [&'a str; 50],
         pub conjunction: &'a str,
         pub negative_prefix: &'a str,
         pub decimal_prefix: &'a str,
         pub decimal_seperator: &'a str,
         pub thousands_seperator: &'a str,
+        pub tens_seperator: &'a str,
     }
 
     impl <'a>NumberFactory<'a> {
         pub fn new(input: &'a str) -> Self {
             Self { 
                 input, 
-                dictionary: ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-                "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
-                "twenty","thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
-                "hundred", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextilion", "septillion",],
-                tens_index: 20,
-                huns_index: 28,
+                dictionary: ["zero", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+                "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                "eighteen", "nineteen", "twenty","thirty", "forty", "fifty",
+                "sixty", "seventy", "eighty", "ninety", "hundred", "thousand",
+                "million", "billion", "trillion", "quadrillion", "quintillion",
+                "sextilion", "septillion", "octillion", "nonillion",
+                "decillion", "undecillion", "duodecillion", "tredecillion",
+                "quattuordecillion", "quindecillion", "sexdecillion", 
+                "septemdecillion", "octodecillion", "novemdecillion", 
+                "vigintillion", ],
                 conjunction: "and",
                 negative_prefix: "negative",
                 decimal_prefix: "point",
                 decimal_seperator: ".",
-                thousands_seperator: ",",       
+                thousands_seperator: ",",
+                tens_seperator: "-",
             }
         }
-        //
-        // State
-        //
-        // get an array of single word numbers from the dictionary
-        fn powers_of_1(&self) -> Vec<&str> { self.dictionary[..self.tens_index].to_vec() }
-        //
-        // get an array of tens words from the dictionary
-        fn powers_of_10(&self) -> Vec<&str> {
-            let mut tens = self.dictionary[self.tens_index..self.huns_index].to_vec();
-            tens.reverse();
-            tens.push(self.dictionary[10]);
-            tens.push("");
-            tens.reverse();
-            tens
-        }
-        // get an array of words for multiples of 1000 from the dictionary
-        fn powers_of_1000(&self) -> Vec<&str> { self.dictionary[self.huns_index..].to_vec() }
+
         //
         // functions
         //
         // check if a negative sign preceeds the input
-        pub fn is_negative(&self, num: &str) -> bool { Regex::new(r"^-+").unwrap().is_match(&num) }
-
+        pub fn is_negative(&self, num: &str) -> bool {
+            Regex::new(r"^-+").unwrap().is_match(&num)
+        }
 
         /*
-        Function to split input based on decimal_seperator into an array of 2 strings
-        with some sensible checking
+        Function to split input based on decimal_seperator into an array of
+        2 strings with some sensible checking
         */ 
         pub fn input_array(&self, num: &str) -> [String; 2] {
-            let s = num.split(self.decimal_seperator).collect::<Vec<&str>>();
+            let s = num.split(self.decimal_seperator)
+                .collect::<Vec<&str>>();
             let mut l = String::new();
             let mut r = String::new();
-            match s.get(0) { Some(l0) => l.push_str(l0), None => {}, }
-            match s.get(1) { Some(r0) => r.push_str(r0), None => {}, }
+            match s.get(0) {
+                Some(l0) => l.push_str(l0),
+                None => {},
+            }
+            match s.get(1) {
+                Some(r0) => r.push_str(r0), 
+                None => {},
+            }
             // first part
             // remove preceeding sign and thousands_seperators
             l = Regex::new(r"^-+").unwrap().replace_all(
-                &Regex::new(self.thousands_seperator).unwrap().replace_all(
-                    &l, 
-                    ""
-                ).to_string(), ""
+                &Regex::new(self.thousands_seperator)
+                .unwrap().replace_all( &l, "")
+                .to_string(), ""
             ).to_string();
             if !Regex::new(r"^\d+$").unwrap().is_match(&l) {
                 // must contain only digits
@@ -81,15 +77,17 @@
                 l = String::from("0")
             } else {
                 // remove preceeding zeros
-                l = Regex::new(r"^0+").unwrap().replace(&l, "").to_string()
+                l = Regex::new(r"^0+").unwrap()
+                    .replace(&l, "")
+                    .to_string()
             }
             // second part
             r = Regex::new(r"0+$").unwrap().replace_all(
-                &Regex::new(self.thousands_seperator).unwrap().replace_all(
-                    &r, 
-                    ""
-                ).to_string(), ""
-            ).to_string();
+                &Regex::new(self.thousands_seperator)
+                .unwrap()
+                .replace_all(&r, "")
+                .to_string(),"")
+                .to_string();
             if !Regex::new(r"^\d+$").unwrap().is_match(&r) {
                 // must contain only digits
                 // panic or empty string
@@ -102,6 +100,13 @@
         Function to split input into groups of 3 characters
         returns a vector array
         will ignore anything after the decimal point
+        for example "1234567" becomes ["567", "234", "1"]
+        1234567 
+        = 1000^0 * 567
+        + 1000^1 * 234
+        + 1000^2 * 1 
+        We are basically determining how big by string manipulation so we dont
+        run into integer overflows
         */
         pub fn thousands(&self, num: &str) -> Vec<String> {
             let n = self.input_array(num);
@@ -125,32 +130,52 @@
         
         pub fn integer_words(&self, num: &str) -> String {
 
-            // function tens
-            // Expects any 2 digit number from 20 to 99 and converts it to words.
-            // Be careful! It does not check the validity of input if you give it any
-            // thing apart from a 2 digit number from 20 to 99, results will be useless.
-            // check the inputs before calling it
+            /*
+            function under_100
+            Expects any 2 digit number from 20 to 99 and converts it to words.
+            Be careful! It does not check the validity of input if you give it
+            any thing apart from a 2 digit number from 20 to 99, results will 
+            be useless.  check the inputs before calling it
+
+            We defined this function as a utility for the next function, 
+            under_1000 since that function will need to call this functionality
+            multiple times
+            */
             let under_100 = | num : &str | {
+                // perform a regular expression match on the input to determine 
+                // if it is less than 100 before using any integer functions
+                // if not raise a panic
                 if Regex::new(r"^.*[^0].{2}$").unwrap().is_match(&num) { 
                     panic!("number must be less than 100");
                 }
+                // ok its a small number less than 100, no integer overflows 
+                // here lets just simply look it up from the dictionary
                 let no = num.parse::<usize>().unwrap();
                 if no < 21 {
-                    self.powers_of_1 ()[no].to_string()
+                    self.dictionary[no].to_string()
                 } else {
-                    let xo = no / 10;
-                    let ox = no % 10;
-                    let yo = self.powers_of_10()[xo].to_string();
+                    let xo = no / 10; // how many tens
+                    let ox = no % 10; // how many units
+                    let yo = self.dictionary[18..][xo].to_string();
                     if ox == 0 {
                         yo
                     } else {
-                        let oy = self.powers_of_1 ()[ox].to_string();
-                        [ yo, oy ].join(" ")
+                        let oy = self.dictionary[ox].to_string();
+                        [ yo, oy ].join(self.tens_seperator)
                     }
                 }
             };
 
+            /*
+            function under_1000
+            Expects any text that can be converted to an integer between 0
+            an 999 and converts to words.  Be careful! It does not check the 
+            validity of input if you give it
+            */
             let under_1000 = | num : &str | {
+                // perform a regular expression match on the input to determine 
+                // if it is less than 1000 before using any integer functions
+                // if not raise a panic
                 if Regex::new(r"^.*[^0].{3}$").unwrap().is_match(&num) { 
                     panic!("number must be less than 1000");
                 }
@@ -160,7 +185,7 @@
                 } else {
                     let xoo = noo / 100;
                     let oxx = noo % 100;
-                    let hun = self.dictionary[self.huns_index].to_string();
+                    let hun = self.dictionary[28].to_string();
                     let yoo = under_100(&xoo.to_string());
                     if oxx == 0 {
                         [ yoo, hun ].join(" ")
@@ -178,22 +203,35 @@
                 under_1000(num0)        
             } else {
                 let mut result = self.thousands(num);
-                let mils = self.powers_of_1000();
+                // let mils = self.powers_of_1000();
                 result.reverse();
 
                 for i in 0..result.len() {
                     let n = &result[i];
-                    if n.parse::<usize>().unwrap() == 0 { result[i] = "".to_string(); continue; };
-                    let w = Regex::new(r"zero").unwrap().replace(&under_1000(&n), "").to_string();
+                    if n.parse::<usize>().unwrap() == 0 {
+                        result[i] = "".to_string(); 
+                        continue;
+                    };
+                    let w = Regex::new(r"zero").unwrap().replace(
+                        &under_1000(&n), 
+                        ""
+                    ).to_string();
                     if i == 0 {
                         if Regex::new(r"0..$").unwrap().is_match(num) &&
                             !Regex::new(r"0+$").unwrap().is_match(num) {
-                            result[i] = format!("{} {}", self.conjunction.to_owned(), &w); 
+                            result[i] = format!("{} {}",
+                                self.conjunction.to_owned(),
+                                &w
+                            ); 
                         } else {
                             result[i] = w;
                         }
                     } else {
-                        let m = if i >  mils.len()-1 { format!("E+{}", i*3) } else { mils[i].to_string() };
+                        let m = if i > self.dictionary[28..].len() - 1 { 
+                            format!("E+{}", i*3) 
+                        } else { 
+                            self.dictionary[28..][i].to_string() 
+                        };
                         result[i] = format!("{} {},", &w, &m);
                     };
                 }
@@ -220,7 +258,7 @@
             } else {
                 let mut dec: String = self.decimal_prefix.to_string();
                 for c in Regex::new(r"0+$").unwrap().replace(num1,"").to_string().chars() {
-                    dec = format!("{} {}", dec, self.powers_of_1()[c.to_digit(10).unwrap() as usize]);
+                    dec = format!("{} {}", dec, self.dictionary[c.to_digit(10).unwrap() as usize]);
                 }
                 dec.trim().to_string()
             }
